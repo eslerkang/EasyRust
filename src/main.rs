0554ff604f742f23a6ec2914e3d0984044e984b0
Taejun Kang <31269150+eslerkang@users.noreply.github.com>
2022-03-06 22:12:01 +0000

    23 - uninitialized variable / for loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+// uninitialized variable
+
+fn loop_then_return(mut counter: i32) -> i32 {
+  // loop but not rustic
+  loop {
+    counter += 1;
+    if counter % 50 == 0 {
+      break;
+    }
+  }
+  counter
+}
+
 fn main() {
-    println!("Hello, world!");
+  let num; // num is uninitialized but not used -> OK
+  {
+    let x = loop_then_return(32);
+    num = x; // num is set by x -> num is num of line 15
+  };
+  println!("{}", num);
 }
