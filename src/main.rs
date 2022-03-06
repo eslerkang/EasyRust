c26c3a689cb7dfd5a69bd03f8ab966e635f93ce1
Taejun Kang <31269150+eslerkang@users.noreply.github.com>
2022-03-06 22:35:36 +0000

    24 lectture

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+// collection types
+// Array
+
+// buffer
+
+#[allow(unused_variables)]
+
 fn main() {
-    println!("Hello, world!");
+  let array = ["One", "Two"]; // [&str; 2]
+  let array2 = ["One", "Two", "Five"]; // [&str; 3] 
+  let array3 = ["One", "Twoo"];
+  // these two arrays are different type
+  println!("Is array the same as array2? {}", array == array3);
+
+  let array4 = [0; 640];
+  println!("{:?}", array4);
+
+  let month = ["1", "2"];
+  println!("{:?}", month.get(1));
+  // Option enum
+  println!("{:?}", month.get(3));
 }
