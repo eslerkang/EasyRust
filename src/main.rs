c1767857b9599a7632c133fb2aa62a56bb2aa164
Taejun Kang <31269150+eslerkang@users.noreply.github.com>
2022-03-06 22:53:34 +0000

    vec![] is sexy macro

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+// Vec - Vector
+// Array = [u8; 10];
+// Vec = Vec<String>
+
+// T = some type
+// generics
+
+#[allow(unused_variables)]
+
 fn main() {
-    println!("Hello, world!");
+  let name1 = String::from("Windy");
+  let name2 = String::from("Gomesy");
+
+  let mut vec = Vec::new();
+  println!("Space for my vec: {}", vec.capacity());
+  vec.push(name1);
+  println!("Space for my vec: {}", vec.capacity());
+  vec.push(name2);
+  // let vec = vec![name1, name2];
+  println!("Space for my vec: {}", vec.capacity());
+  println!("{:?}", vec);
 }
