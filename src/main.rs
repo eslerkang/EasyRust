cdfd2d4a205c36bd12f3dd377b2505a12d701530
Taejun Kang <31269150+eslerkang@users.noreply.github.com>
2022-03-06 23:05:50 +0000

    about trait(a little bit) and from/into - check out the official doc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+// trait = 초능력
+// This type implements (trait name)
+
+// From, Into
+
 fn main() {
-    println!("Hello, world!");
+  let my_name = String::from("Kang Taejun");
+  let my_city: String = "Seoul".into();
+  
+  println!("{}", my_city);
 }
