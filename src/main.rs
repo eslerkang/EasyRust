21307940c7488de296c58f6b6d86e770d40ef679
Taejun Kang <31269150+eslerkang@users.noreply.github.com>
2022-03-06 22:44:40 +0000

    lecture / example about slices

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+// slices
+// vecs -> vector - next lecture
+// &str
+
+// Array Vec
+// &str String
+// String ==> Vec<u8>
+
+// dynamically sized type
+
 fn main() {
-    println!("Hello, world!");
+  let seasons = ["Spring", "Summer", "Fall", "Winter", "Spring", "Summer", "Fall", "Winter"];
+  println!("{:?}", &seasons[..=4]);
 }
