50ae9a392ac3ad70ddec11da7b659d0dae8d2030
Taejun Kang <31269150+eslerkang@users.noreply.github.com>
2022-03-06 23:41:07 +0000

    match is better than switch

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+// control flow
+
 fn main() {
-    println!("Hello, world!");
+  let num = 7;
+  let num2 = 10;
+  if num == 7 && num2 == 10{
+    println!("They both match!!");
+  } else if num == 6 { // && = and, || = or
+    println!("It's six");
+  } else {
+    println!("What are you!??");
+  }
+
+  // expression based language
+  let my_num = 5u8;
+  let second_num = match my_num { // better than switch
+    0 => 23,
+    1 => 234,
+    _ => 0,
+  };
+  println!("second is {}", second_num);
 }
