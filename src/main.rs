1d5e1c884b15d79562f72e3f3ff1865b6fb152ed
Taejun Kang <31269150+eslerkang@users.noreply.github.com>
2022-03-08 06:38:29 +0000

    size of struct

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
+use std::mem::size_of_val;
+
+struct numbers {
+  one: u8, // 1
+  two: u8, // 1
+  three: u8, // 1
+  four: u32 // 4
+  // 4 + 4 -> two 4bytes
+}
+
 #[derive(Debug)]
 struct Country {
   population: u32,
   capital: String,
-  leader: String
+  leader: String,
+  all_populations: [u32; 5500]
 }
   
 fn main() {
@@ -12,7 +23,18 @@ fn main() {
   let country = Country {
     population,
     capital,
-    leader
+    leader,
+    all_populations: [500; 5500]
   };
-  println!("{:#?}", country)
+  println!("{:#?}", country);
+  println!("size of country: {}", size_of_val(&country));
+
+  let number = numbers{
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 32
+  };
+
+  println!("size of number: {}", size_of_val(&number));
 }
