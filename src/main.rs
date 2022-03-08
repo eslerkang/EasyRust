21de1183a23de6ede8274f4d378df867de850ffc
Taejun Kang <31269150+eslerkang@users.noreply.github.com>
2022-03-08 03:39:44 +0000

    three types of struct / names struct is what I think is structic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+// struct -> my own type / and
+// enum / or
+
+// unit struct
+struct FileDirectory;
+// tuple struct
+#[derive(Debug)]
+struct Color(u8, u8, u8);
+// named struct
+#[derive(Debug)]
+struct Country {
+  population: u32,
+  capital: String,
+  leader_name: String
+}
+
+fn takes_file_directory(_input: FileDirectory) {
+  println!("haha");
+}
+
 fn main() {
-    println!("Hello, world!");
+  let x = FileDirectory;
+  println!("{}", std::mem::size_of_val(&x));
+  
+  let color = Color(20, 20, 100);
+  
+  println!("{:?}", color);
+  let korea = Country {
+    population: 50_000_000,
+    capital: String::from("seoul"),
+    leader_name: String::from("Moon")
+  };
+  println!("{:#?}", korea);
 }
