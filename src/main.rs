1b31c010120b5821b8fd4e51d6c04f36b928d680
Taejun Kang <31269150+eslerkang@users.noreply.github.com>
2022-03-08 06:31:17 +0000

    make struct with concept of js

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#[derive(Debug)]
+struct Country {
+  population: u32,
+  capital: String,
+  leader: String
+}
+  
 fn main() {
-    println!("Hello, world!");
+  let population = 50_000_000;
+  let capital = "Seoul".to_string();
+  let leader = "Moon".to_string();
+  let country = Country {
+    population,
+    capital,
+    leader
+  };
+  println!("{:#?}", country)
 }
