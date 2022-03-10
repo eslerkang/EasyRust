9c4383d122d9d9436b775567cf0338ccfe401031
Taejun Kang <31269150+eslerkang@users.noreply.github.com>
2022-03-10 08:39:54 +0000

    loop, while, for expressions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
 fn main() {
-    println!("Hello, world!");
+  let mut counter = 0;
+  let mut counter2 = 0;
+  'first: loop {
+    counter += 1;
+    println!("the counter is: {}", counter);
+    if counter > 9 {
+      println!("now entering the second loop");
+
+      'second: loop {
+        println!("second counter is: {}", counter2);
+        counter2 += 1;
+        if counter2 == 3 {
+          break 'first;
+        }
+      }
+    }
+  }
+
+  counter = 0;
+  while counter != 5 {
+    counter += 1;
+    println!("{}", counter);
+  }
+
+  for _number in 0..=3 {
+    // 0..3 => 0 1 2
+    // 0..=3 => 0 1 2 3
+    println!("I don't need number");
+  }
+
+  // break can have return value
+  counter = 5;
+
+  let number = loop {
+    counter += 1;
+    if counter %53 == 3 {
+      break counter;
+    }
+  };
+  println!("number is now: {}", number);
 }
