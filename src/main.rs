298497a4b55d2a13c94d83aa109267c4de585e00
Taejun Kang <31269150+eslerkang@users.noreply.github.com>
2022-03-14 06:34:27 +0000

    deref of dot operation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+// .
+// .
+
+struct Item {
+  num: u8
+}
+
+// .dot operator
+impl Item {
+  fn compare_number(&self, other_num: u8) {
+    println!("{}", self.num == other_num);
+  }
+}
+
+// Deref = *
 fn main() {
-    println!("Hello, world!");
+  let item = Item {
+    num: 10
+  };
+  let refer = &item.num; // u8
+  let refer_item = &item; // &Item
+  let refrefer_item = &refer_item; // &&Item -> **
+  item.compare_number(10);
+  refer_item.compare_number(10);
+  refrefer_item.compare_number(10);
+
+  println!("Are they the same? {}", *refer == 10u8);
 }
