579b2e6a25480caccbca5b5903afecc0082f579f
Taejun Kang <31269150+eslerkang@users.noreply.github.com>
2022-03-14 06:23:37 +0000

    destructuring

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+// destructuring
+
+struct Person {
+    name: String,
+    real_name: String,
+    height: u8,
+    happiness: bool,
+}
+
+#[derive(Debug)]
+struct Person2 {
+    name: String,
+    height: u8,
+}
+
+impl Person2 {
+    fn from_person(input: Person) -> Self {
+        let Person { name, height, .. } = input;
+        Self { name, height }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let papa_doc = Person {
+        name: "Papa Doc".to_string(),
+        real_name: "Clarence".to_string(),
+        height: 170,
+        happiness: false,
+    };
+    let person2 = Person2::from_person(papa_doc);
+    println!("{:?}", person2);
 }
