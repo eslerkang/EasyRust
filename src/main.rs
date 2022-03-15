4b1bc0355ff935a675d99141d0bf78872ae9cd84
Taejun Kang <31269150+eslerkang@users.noreply.github.com>
2022-03-15 11:39:59 +0000

    generic and restricts on it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::fmt::Display;
+use std::cmp::PartialOrd;
+
+// generics - concrete
+// generic - 일반적인 / 대중적인
+
+struct Book {
+  year: u16
+}
+
+fn give_thing<T: Display>(input: T) -> T { // T
+  println!("{}", input); // Display
+  input
+}
+
+fn compare_and_print<
+  T: Display, 
+  U: Display + PartialOrd
+  >(statement: T, num1: U, num2: U) {
+  println!("{}! Is {} greater than {}? => {}", statement, num1, num2, num1 > num2);
+}
+
 fn main() {
-    println!("Hello, world!");
+  let x = give_thing(33);
+  let y = give_thing(String::from("haha"));
+  println!("{} {}", x, y);
+  // let book = give_thing(Book{year: 11}); // cannot be executed
+
+  compare_and_print("Listen up!", 9, 8);
 }
